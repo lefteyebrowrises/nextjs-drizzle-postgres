852033cc11e50eb77c21741e6435c8e530fe568d
Achmad Alpian <58936609+lefteyebrowrises@users.noreply.github.com>
2024-01-17 10:13:19 +0700

    use vercel/postgres
    replace postgres js with vercel/postgres

--- app/db.ts
+++ app/db.ts
@@ -1,16 +1,15 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
+import { drizzle } from 'drizzle-orm/vercel-postgres';
 import { pgTable, serial, varchar } from 'drizzle-orm/pg-core';
 import { eq } from 'drizzle-orm';
-import postgres from 'postgres';
+import { sql } from '@vercel/postgres';
 import { genSaltSync, hashSync } from 'bcrypt-ts';
 
 // Optionally, if not using email/pass login, you can
 // use the Drizzle adapter for Auth.js / NextAuth
 // https://authjs.dev/reference/adapter/drizzle
-let client = postgres(`${process.env.POSTGRES_URL!}?sslmode=require`);
-let db = drizzle(client);
+let db = drizzle(sql);
 
-let users = pgTable('User', {
+let users = pgTable('users', {
   id: serial('id').primaryKey(),
   email: varchar('email', { length: 64 }),
   password: varchar('password', { length: 64 }),
